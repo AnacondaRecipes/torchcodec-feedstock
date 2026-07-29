000e9e81e07db1d849909dcba5dc8b25d5d51250
Andrew Howe <ahowe@anaconda.com>
2026-07-29 11:47:21 -0400

    cleanup
    * moving CUDA variables to the correct case in the build scripts
    * removing commente out sections of meta.yaml
    * adding torch version to the build string - precautionary in case we
      have the need to build against multiple torch versions

--- recipe/build_torchcodec.sh
+++ recipe/build_torchcodec.sh
@@ -3,10 +3,10 @@ set -ex
 
 if [[ ${gpu_variant} != "cuda" ]]; then
    export ENABLE_CUDA=0
-   export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
-   export CUDACXX="${BUILD_PREFIX}/bin/nvcc"
 else
    export ENABLE_CUDA=1
+   export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
+   export CUDACXX="${BUILD_PREFIX}/bin/nvcc"
 fi
 
 
